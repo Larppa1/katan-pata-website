0f4f36640af7949bec8da042f868a3ed84dbb52c
Larppa1 <lari.juva7@gmail.com>
2022-10-24 11:12:14 +0300

    -

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 import './Contact.css'
 import DesktopHeader from '../../components/desktop/DesktopHeader'
 import MobileHeader from '../../components/mobile/MobileHeader'
 import Footer from '../../components/universal/Footer'
 
 export default function Contact() {
+    const form = useRef()
+    const sendEmail = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('service_hh87ikt', 'template_pquqfst', form.current, 'n5MdbvlDxAKYVvX49')
+            .then((result) => {
+                console.log(result.text)
+                form.current.submit()
+            }, (error) => {
+                console.log(error.text)
+            })
+    }
+
     if(window.innerWidth < 768) {
         return(
             <div className="container">
@@ -19,6 +32,16 @@ export default function Contact() {
                                 040 56 44 049<br/>
                                 katanpata@gmail.com
                             </h2>
+                        </article>
+                    </section>
+                    <section>
+                        <article id='feedbackContainer' className='prose'>
+                            <h2>Anna palautetta!</h2>
+                            <h4>Haluamme kuulla, miten olemme onnistuneet palveluiden tarjonnassa. Kaikki palautteet ovat nimettömiä, eikä niitä voida yhdistää kehenkään henkilöön.</h4>
+                            <form ref={form} onSubmit={sendEmail}>
+                                <textarea id='message' className="textarea textarea-bordered" placeholder="Viesti" name="message"></textarea>
+                                <button className='btn' type='submit'>Lähetä</button>
+                            </form>
                         </article>
                     </section>
                 </main>
@@ -41,6 +64,16 @@ export default function Contact() {
                                 040 56 44 049<br/>
                                 katanpata@gmail.com
                             </h2>
+                        </article>
+                    </section>
+                    <section>
+                        <article id='feedbackContainer' className='prose'>
+                            <h2>Anna palautetta!</h2>
+                            <h4>Haluamme kuulla, miten olemme onnistuneet palveluiden tarjonnassa. Kaikki palautteet ovat nimettömiä, eikä niitä voida yhdistää kehenkään henkilöön.</h4>
+                            <form ref={form} onSubmit={sendEmail}>
+                                <textarea id='message' className="textarea textarea-bordered" placeholder="Viesti" name="message"></textarea>
+                                <button className='btn' type='submit'>Lähetä</button>
+                            </form>
                         </article>
                     </section>
                 </main>
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,42 +1,12 @@
-import React, { useRef } from 'react'
-import emailjs from '@emailjs/browser'
 import './Landing.css'
 import DesktopHeader from '../../components/desktop/DesktopHeader'
 import MobileHeader from '../../components/mobile/MobileHeader'
 import Footer from '../../components/universal/Footer'
 
 export default function Landing() {
-    const form = useRef()
-
-    const sendEmail = (e) => {
-        e.preventDefault()
-        emailjs.sendForm('service_hh87ikt', 'template_pquqfst', form.current, 'n5MdbvlDxAKYVvX49')
-            .then((result) => {
-                console.log(result.text)
-                form.current.submit()
-            }, (error) => {
-                console.log(error.text)
-            })
-    }
-
     if(window.innerWidth < 768) {
         return(
             <div className="container">
-                <section>
-                    <div className="dropdown dropdown-top dropdown-end">
-                        <label tabIndex={0} className="btn bg-base-300 shadow-xl">Palaute</label>
-                        <div className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary rounded-box w-80">
-                            <article id='feedbackContainer' className='prose'>
-                                <h2>Anna palautetta!</h2>
-                                <h4>Haluamme kuulla, miten olemme onnistuneet palveluiden tarjonnassa. Kaikki palautteet ovat nimettömiä, eikä niitä voida yhdistää kehenkään henkilöön.</h4>
-                                {/* <form ref={form} onSubmit={sendEmail}>
-                                    <textarea id='message' className="textarea textarea-bordered" placeholder="Viesti" name="message"></textarea>
-                                    <input type="submit" className="btn">Lähetä</input>
-                                </form> */}
-                            </article>
-                        </div>
-                    </div>
-                </section>
                 <header>
                     <MobileHeader />
                 </header>
@@ -65,21 +35,6 @@ export default function Landing() {
     }else {
         return(
             <div className="container">
-                <section>
-                    <div className="dropdown dropdown-top dropdown-end">
-                        <label tabIndex={0} className="btn bg-base-300 shadow-xl">Palaute</label>
-                        <div className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary rounded-box w-96">
-                            <article id='feedbackContainer' className='prose'>
-                                <h2>Anna palautetta!</h2>
-                                <h4>Haluamme kuulla, miten olemme onnistuneet palveluiden tarjonnassa. Kaikki palautteet ovat nimettömiä, eikä niitä voida yhdistää kehenkään henkilöön.</h4>
-                                <form ref={form} onSubmit={sendEmail}>
-                                    <textarea id='message' className="textarea textarea-bordered" placeholder="Viesti" name="message"></textarea>
-                                    <button className="btn">Lähetä</button>
-                                </form>
-                            </article>
-                        </div>
-                    </div>
-                </section>
                 <header>
                     <DesktopHeader />
                 </header>
